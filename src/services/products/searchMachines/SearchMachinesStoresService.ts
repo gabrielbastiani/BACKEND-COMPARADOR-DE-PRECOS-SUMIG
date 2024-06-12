00aac27f722ed142ccb39ec2cf06c82bc73a93df
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2024-06-12 13:45:15 -0300

    front

--- src/services/products/searchMachines/SearchMachinesStoresService.ts
+++ src/services/products/searchMachines/SearchMachinesStoresService.ts
@@ -10,7 +10,9 @@ interface SearchRequest {
 class SearchMachinesStoresService {
     async execute({ urlSearchStore, stores }: SearchRequest) {
 
+        let hasNextPage = true;
         const list_products: any = [];
+        let attempts = 0;
 
         const browser = await puppeteer.launch({
             headless: false
@@ -27,98 +29,122 @@ class SearchMachinesStoresService {
 
         try {
 
-            await page.waitForSelector('.oR27Gd', { timeout: 60000 });
+            while (hasNextPage && attempts < 4) {
 
-            const images = await page.$$eval('.oR27Gd > img', (el: any[]) => el.map((link: { src: any; }) => link.src));
+                await page.waitForSelector('.oR27Gd', { timeout: 60000 });
 
-            await page.waitForSelector('.rgHvZc', { timeout: 60000 });
+                const images = await page.$$eval('.oR27Gd > img', (el: any[]) => el.map((link: { src: any; }) => link.src));
 
-            const links = await page.$$eval('.rgHvZc > a', (el: any[]) => el.map((link: { href: any; }) => link.href));
+                await page.waitForSelector('.rgHvZc', { timeout: 60000 });
 
-            const title = await page.$$eval(`.rgHvZc > a`, elementos => {
-                return elementos.map(elemento => elemento.textContent.trim());
-            });
+                const links = await page.$$eval('.rgHvZc > a', (el: any[]) => el.map((link: { href: any; }) => link.href));
 
-            await page.waitForSelector('.HRLxBb', { timeout: 60000 });
+                const title = await page.$$eval(`.rgHvZc > a`, elementos => {
+                    return elementos.map(elemento => elemento.textContent.trim());
+                });
 
-            const price = await page.$$eval('.HRLxBb', elementos => {
-                return elementos.map(elemento => elemento.textContent.trim());
-            });
+                await page.waitForSelector('.HRLxBb', { timeout: 60000 });
 
-            function processarString(str: string) {
-                if (str.includes('.')) {
-                    str = str.replace('.', '');
+                const price = await page.$$eval('.HRLxBb', elementos => {
+                    return elementos.map(elemento => elemento.textContent.trim());
+                });
+
+                function processarString(str: string) {
+                    if (str.includes('.')) {
+                        str = str.replace('.', '');
+                    }
+
+                    str = str.replace(/R\$\s*/g, '').replace(/,/g, '.');
+
+                    return str;
                 }
 
-                str = str.replace(/R\$\s*/g, '').replace(/,/g, '.');
+                const brand: any = [];
 
-                return str;
-            }
+                for (let i = 0; i < title.length; i++) {
+                    const palavras = title[i].split(' ');
+                    const brands = palavras[palavras.length - 1];
 
-            const brand: any = [];
+                    brand.push(brands);
+                }
 
-            for (let i = 0; i < title.length; i++) {
-                const palavras = title[i].split(' ');
-                const brands = palavras[palavras.length - 1];
+                const store = stores;
 
-                brand.push(brands);
-            }
+                const obj: { [key: string]: any } = {};
+                obj.array1 = title;
+                obj.array2 = price;
+                obj.array3 = brand;
+                obj.array4 = links;
+                obj.array5 = images;
 
-            const store = stores;
+                const news = Object.keys(obj.array1).map((index) => ({
+                    store: store,
+                    image: obj.array5[index],
+                    title: obj.array1[index],
+                    price: Number(processarString(obj.array2[index])),
+                    brand: obj.array3[index],
+                    link: obj.array4[index]
+                }));
 
-            const obj: { [key: string]: any } = {};
-            obj.array1 = title;
-            obj.array2 = price;
-            obj.array3 = brand;
-            obj.array4 = links;
-            obj.array5 = images;
+                function removerAcentos(s: any) {
+                    return s.normalize('NFD')
+                        .replace(/[\u0300-\u036f]/g, "")
+                        .toLowerCase()
+                        .replace(/ +/g, "-")
+                        .replace(/-{2,}/g, "-")
+                        .replace(/[/]/g, "-");
+                }
 
-            const news = Object.keys(obj.array1).map((index) => ({
-                store: store,
-                image: obj.array5[index],
-                title: obj.array1[index],
-                price: Number(processarString(obj.array2[index])),
-                brand: obj.array3[index],
-                link: obj.array4[index]
-            }));
+                function removerAcentosTitle(s: any) {
+                    return s.normalize('NFD')
+                        .replace(/[\u0300-\u036f]/g, "")
+                        .toLowerCase()
+                        .replace(/ +/g, "-")
+                        .replace(/-{2,}/g, "-")
+                        .replace(/[/]/g, "-");
+                }
 
-            function removerAcentos(s: any) {
-                return s.normalize('NFD')
-                    .replace(/[\u0300-\u036f]/g, "")
-                    .toLowerCase()
-                    .replace(/ +/g, "-")
-                    .replace(/-{2,}/g, "-")
-                    .replace(/[/]/g, "-");
-            }
+                for (const item of news) {
+                    await prismaClient.storeProduct.create({
+                        data: {
+                            store: item.store,
+                            slug: removerAcentos(item.store),
+                            link_search: urlSearchStore,
+                            image: item.image,
+                            title_product: item.title,
+                            slug_title_product: removerAcentosTitle(item.title),
+                            price: item.price,
+                            brand: item.brand.replace(/\|/g, ''),
+                            link: item.link
+                        }
+                    });
+                }
 
-            function removerAcentosTitle(s: any) {
-                return s.normalize('NFD')
-                    .replace(/[\u0300-\u036f]/g, "")
-                    .toLowerCase()
-                    .replace(/ +/g, "-")
-                    .replace(/-{2,}/g, "-")
-                    .replace(/[/]/g, "-");
-            }
+                list_products.push(news);
 
-            for (const item of news) {
-                await prismaClient.storeProduct.create({
-                    data: {
-                        store: item.store,
-                        slug: removerAcentos(item.store),
-                        link_search: urlSearchStore,
-                        image: item.image,
-                        title_product: item.title,
-                        slug_title_product: removerAcentosTitle(item.title),
-                        price: item.price,
-                        brand: item.brand.replace(/\|/g, ''),
-                        link: item.link
+                const nextButton = await page.$('.u30d4 > a');
+
+                console.log(nextButton)
+
+                if (nextButton) {
+                    const currentURL = page.url();
+                    await Promise.all([
+                        page.waitForNavigation({ waitUntil: 'networkidle0' }),
+                        nextButton.click(),
+                    ]);
+                    const newURL = page.url();
+                    if (currentURL === newURL) {
+                        hasNextPage = false;
                     }
-                });
+                } else {
+                    hasNextPage = false;
+                }
+
+                attempts++;
+
             }
 
-            list_products.push(news);
-
-            await browser.close();
+            /* await browser.close(); */
 
             return list_products[0];
 
